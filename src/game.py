fde5027707d13d1c3b873eb58fd50567ea42878a
majabss <maja.brink@web.de>
2017-11-27 21:07:05 +0100

    part 1

--- src/game.py
+++ src/game.py
@@ -1,56 +1,32 @@
 import pygame
-import sys
-import math
-import random
 
-import player
-import camera
-import block
-import level
-import input_manager
-import inventory
-import draw
+# has to be the first call!
+pygame.init()
+# surface
+gameDisplay = pygame.display.set_mode((800, 600))
+# window title
+pygame.display.set_caption('A bit Racey')
+# game clock
+clock = pygame.time.Clock()
 
 
-""" Main game loop """
-def run_game(screen, s_height, s_width):
+crashed = False
+# game loop
+while not crashed:
 
-    FPS = 60
-    clock = pygame.time.Clock()
-    game_running = True
+    # list of events per frame per second:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # player wanted to close the window
+            crashed = True
 
-    level_manager = level.LevelManager()
-    level_manager.set_level(0)
+        print(event)
 
-    cam = camera.Camera(s_height, s_width)
-    level_renderer = draw.Renderer(screen, level_manager.curr_level, cam)
+    # update eyerything or only the one parameter
+    pygame.display.update()
 
-    input_handler = input_manager.InputManager()
+    #  frames per second
+    clock.tick(60)
 
-    # Game loop
-    while game_running:
-
-        delta = clock.tick(FPS) / 1000
-
-        level_renderer.clear()
-        curr_input = input_handler.get_input() 
-        level_manager.curr_level.update(curr_input, delta)
-        cam.update(level_manager.curr_level.player)
-        level_renderer.draw_level()
-
-
-def screen_setup():
-    SCREEN_WIDTH = 640
-    SCREEN_HEIGHT = 480
-    window_mode = pygame.FULLSCREEN
-
-    pygame.init()
-    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT], window_mode)
-    pygame.display.set_caption("Cool game 0.1.0")
-    
-    return screen, SCREEN_WIDTH, SCREEN_HEIGHT
-
-
-if __name__ == '__main__':
-    screen, s_width, s_height = screen_setup()
-    run_game(screen, s_width, s_height)
+pygame.quit()
+quit()
